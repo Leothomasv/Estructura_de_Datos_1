decaf3336e44b7411cf0f36e144b0eabfd74369c
LEONEL ALBERTO THOMAS VILLALOBOS <leothomasv@unitec.edu>
2019-10-16 17:01:30 -0600

    nuevo

--- Comb_Sort/CombSort.cpp
+++ Comb_Sort/CombSort.cpp
@@ -2,39 +2,28 @@
 #include <iostream> 
 using namespace std;
 
-// To find gap between elements 
+// encontrar el espacio entre elementos 
 int getNextGap(int gap)
 {
-	// Shrink gap by Shrink factor 
+	// reducir el espacio
 	gap = (gap * 10) / 13;
-
 	if (gap < 1)
 		return 1;
 	return gap;
 }
 
-// Function to sort a[0..n-1] using Comb Sort 
 void combSort(int a[], int n)
 {
-	// Initialize gap 
 	int gap = n;
-
-	// Initialize swapped as true to make sure that 
-	// loop runs 
 	bool swapped = true;
 
-	// Keep running while gap is more than 1 and last 
-	// iteration caused a swap 
 	while (gap != 1 || swapped == true)
 	{
-		// Find next gap 
+		// encontrar el siguiente espacio
 		gap = getNextGap(gap);
-
-		// Initialize swapped as false so that we can 
-		// check if swap happened or not 
 		swapped = false;
 
-		// Compare all elements with current gap 
+		// Comparar todos los elementos
 		for (int i = 0; i < n - gap; i++)
 		{
 			if (a[i] > a[i + gap])
@@ -46,7 +35,6 @@ void combSort(int a[], int n)
 	}
 }
 
-// Driver program 
 int main()
 {
 	int a[] = { 8, 4, 1, 56, 3, -44, 23, -6, 28, 0 };
@@ -54,7 +42,7 @@ int main()
 
 	combSort(a, n);
 
-	printf("Sorted array: \n");
+	printf("Arreglo Ordenado: \n");
 	for (int i = 0; i < n; i++)
 		printf("%d ", a[i]);
 
