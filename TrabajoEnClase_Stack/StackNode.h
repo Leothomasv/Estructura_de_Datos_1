4c7324c185b33cd39ea8b5ceca95a54ec34143c6
LEONEL ALBERTO THOMAS VILLALOBOS <leothomasv@unitec.edu>
2019-10-30 23:37:36 -0600

    nuevo

--- TrabajoEnClase_Stack/StackNode.h
+++ TrabajoEnClase_Stack/StackNode.h
@@ -1,6 +1,9 @@
 #pragma once
 #ifndef STACKNODE_H
 #define STACKNODE_H
+#include <iostream>
+
+using namespace std;
 
 template <class T> 
 
@@ -9,14 +12,8 @@ struct  StackNode
 	T dato;
 	StackNode* siguiente;
 
-	StackNode(T dato, siguiente nullptr) {
-
-		dato = 0;
-		siguiente = nullptr;
-	}
-
 };
-
+	
 template <class T>
 
 class Stack {
@@ -29,9 +26,59 @@ public:
 	void push(const T&);
 	void pop(void);
 	const T& peek(void);
+	void imprimir();
 };
 
 
+template <class T>
+Stack<T>::Stack():Stack<T>::primero(nullptr) {}
+
+template <class T>
+bool Stack<T>::estaVacia(void) {
+	return primero == nullptr;
+}
+
+template <class T>
+void Stack<T>::push(const T& dato) {
+	StackNode<T>* nuevo = new StackNode<T>;
+
+	nuevo->siguiente = nullptr;
+    nuevo->dato = dato;
+
+	if (estaVacia()) {
+		primero = nuevo;
+	}
+	else {
+		nuevo->siguiente = primero;
+		primero = nuevo;
+	}
+}
+
+template <class T>
+void Stack<T>::pop(void) {
+	if (!estaVacia()) {
+
+		StackNode<T>* borrar = primero;
+		primero = primero->siguiente;
+		delete borrar;
+	}
+}
+
+template <class T>
+const T& Stack<T>::peek(void) {
+	StackNode<T>* nuevo = primero;
+	return nuevo->dato;
+}
+
+template <class T>
+void Stack<T>::imprimir(void) {
+	StackNode<T>* actual = primero;
+
+	while (actual != nullptr) {
+		cout << "{"<< actual->dato << "}" << endl;
+		actual = actual->siguiente;
+	}
+}
 #endif // !STACKNODE_H
 
 
--- TrabajoEnClase_Stack/main.cpp
+++ TrabajoEnClase_Stack/main.cpp
@@ -5,5 +5,22 @@ using namespace std;
 
 int main() {
 
+	Stack<int>* numero = new Stack<int>;
+
+	numero->push(8);
+	numero->push(5);
+	numero->push(2);
+	numero->push(0);
+	numero->push(1);
+	numero->push(9);
+
+	numero->imprimir();
+
+	numero->pop();
+	cout << "Impresion despues del pop\n";
+	numero->imprimir();
+
+
+	cout<< "Primero numero de la lista: "<< numero->peek();
 
 }
